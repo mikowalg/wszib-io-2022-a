44b9491f496c724792cd92f7355596ceb3df5d78
mikowalg <mikowalg@student.wszib.edu.pl>
2022-05-15 17:20:11 +0200

    Dział optymalizacji: usunięcie niepotrzebnego kodu

--- trojkaty.java
+++ trojkaty.java
@@ -10,18 +10,8 @@ class Trojkaty {
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
-<<<<<<< HEAD
          if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
-||||||| 3c81867
-         if (a == b && b == c && a == c) {
-             System.out.println("Trójkąt równoboczny"); 
-=======
-         if (a == b && b == c && a == c) {
-             System.out.println("Trójkąt równoboczny");
-
->>>>>>> main
-         }
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
     /** Wyświetla ekran pomocy */
@@ -39,10 +29,6 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
-
-	if (a < 0 || b < 0 || c < 0) {
-	  System.out.println("Długości boków trójkąta muszą być nieujemne!");
-	  }
         
         jakiTrojkat(a, b, c);
     }
